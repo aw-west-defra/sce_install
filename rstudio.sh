93a83386c3b5c4b96a3c9905711dc091cd44587f
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2022-05-09 12:57:27 +0100

    Update rstudio.sh

--- rstudio.sh
+++ rstudio.sh
@@ -1,77 +1,55 @@
+# Install R (from binary)
+REPO="deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+URL="https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc"
+FILE="/etc/apt/trusted.gpg.d/cran_ubuntu_key.asc"
+
+sudo add-apt-repository -y $REPO
 sudo apt update
-sudo apt install -y --no-install-recommends software-properties-common dirmngr
-
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-sudo apt install --no-install-recommends r-base
-
-# Ubuntu CRAN binaries
-sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+ -y 
-sudo apt update && sudo apt install r-cran-tidyverse  # Not necessary, proves the repo was registered
+wget $URL -aO $FILE
+sudo apt install -y --no-install-recommends \
+  r-base
 
 
-RSTUDIO_VERSION="stable"
-ARCH=$(dpkg --print-architecture)
-DOWNLOAD_FILE=rstudio-server.deb
-R_HOME=/usr/local/lib/R
+# Install Tidyverse
+REPO="ppa:c2d4u.team/c2d4u4.0+"
 
-sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-    file \
-    git \
-    libapparmor1 \
-    libgc1c2 \
-    libclang-dev \
-    libcurl4-openssl-dev \
-    libedit2 \
-    libobjc4 \
-    libssl-dev \
-    libpq5 \
-    lsb-release \
-    psmisc \
-    procps \
-    python-setuptools \
-    pwgen \
-    sudo \
-    wget
+sudo add-apt-repository -y $REPO
+sudo apt update
+sudo apt install -y \
+  r-cran-tidyverse
 
-wget "https://rstudio.org/download/latest/${RSTUDIO_VERSION}/server/bionic/rstudio-server-latest-${ARCH}.deb" \
-    -O "$DOWNLOAD_FILE" \
-  && sudo dpkg -i "$DOWNLOAD_FILE" \
-  && rm "$DOWNLOAD_FILE"
 
-## Set up RStudio init scripts
-sudo mkdir -p /etc/services.d/rstudio
+# Install RStudio (from binary)
+URL="https://rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-$(dpkg --print-architecture).deb"
+FILE="rstudio-server.deb"
 
-# Run file
-echo '#!/usr/bin/with-contenv bash
+wget $URL -O $FILE
+sudo dpkg -i $FILE
+rm $FILE
+
+
+# Setup RStudio
+STR_RUN='#!/usr/bin/with-contenv bash
 ## load /etc/environment vars first:
 for line in $( cat /etc/environment ) ; do export $line > /dev/null; done
-exec /usr/lib/rstudio-server/bin/rserver --server-daemonize 0' \
-> /tmp/rstudio-run \
-  && sudo mv /tmp/rstudio-run /etc/services.d/rstudio/run
-
-# Finish file
-echo '#!/bin/bash
-rstudio-server stop' \
-> /tmp/rstudio-finish \
-  && sudo mv /tmp/rstudio-finish /etc/services.d/rstudio/finish
-
-
-# Logging file to stderr
-LOGGING="[*]
+exec /usr/lib/rstudio-server/bin/rserver --server-daemonize 0'
+STR_FINISH='#!/bin/bash
+rstudio-server stop'
+STR_LOG='[*]
 log-level=warn
-logger-type=syslog
-"
+logger-type=syslog'
 
-printf "%s" "$LOGGING" > /tmp/rstudio-logging && sudo mv /tmp/rstudio-logging /etc/rstudio/logging.conf
-  
-  
-  sudo apt update && sudo apt install -y \
+sudo mkdir -p /etc/services.d/rstudio
+sudo echo $STR_RUN > /etc/services.d/rstudio/run
+sudo echo $STR_FINISH > /etc/services.d/rstudio/finish
+sudo echo $STR_LOG > /etc/rstudio/logging.conf
+
+
+# Install Python
+sudo apt update
+sudo apt install -y \
   software-properties-common \
   python3-dev \
   python3-pip \
   build-essential
-
-# I don't currently recommend using `venv` over `virtualenv` due to
-# bug between Ubuntu and pip which causes an incorrect `pip freeze` output
 pip3 install -U virtualenv
